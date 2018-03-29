9f4794ec171335707e26fa896142beb463cafac6
TIN2539 <TIN2539@users.noreply.github.com>
2018-03-29 23:37:03 +0300

    Close #5

--- TitleCapitalizationTool/Program.cs
+++ TitleCapitalizationTool/Program.cs
@@ -27,99 +27,107 @@ namespace TitleCapitalizationTool
 					userString = String.Join(" ", args);
 				}
 
-				userString = userString.ToLower();
-
-				string[] words = userString.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-				userString = String.Join(" ", words);
-
-				StringBuilder builder = new StringBuilder(userString);
-				for (int i = 0; i < builder.Length; i++) //Insert ' ' after punctuation marks
+				if(userString.Length > 0)
 				{
-					for (int x = 0; x < punctuationMarks.Length; x++)
+					userString = userString.ToLower();
+
+					string[] words = userString.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+					userString = String.Join(" ", words);
+
+					StringBuilder builder = new StringBuilder(userString);
+					for (int i = 0; i < builder.Length; i++) //Insert ' ' after punctuation marks
 					{
-						if (builder[i] == punctuationMarks[x] && i + 1 < builder.Length && builder[i + 1] != ' ')
+						for (int x = 0; x < punctuationMarks.Length; x++)
 						{
-							builder.Insert(i + 1, ' ');
-							break;
+							if (builder[i] == punctuationMarks[x] && i + 1 < builder.Length && builder[i + 1] != ' ')
+							{
+								builder.Insert(i + 1, ' ');
+								break;
+							}
 						}
 					}
-				}
-				for (int i = 0; i < builder.Length; i++) //Remove ' ' before punctuation marks
-				{
-					for (int x = 0; x < punctuationMarks.Length; x++)
+					for (int i = 0; i < builder.Length; i++) //Remove ' ' before punctuation marks
 					{
-						if (builder[i] == punctuationMarks[x] && builder[i - 1] == ' ')
+						for (int x = 0; x < punctuationMarks.Length; x++)
 						{
-							builder.Remove(i - 1, 1);
-							break;
+							if (builder[i] == punctuationMarks[x] && builder[i - 1] == ' ')
+							{
+								builder.Remove(i - 1, 1);
+								break;
+							}
 						}
 					}
-				}
 
-				for (int i = 0; i < builder.Length; i++) //Insert ' ' before and after '-'
-				{
-					if (builder[i] == '-')
+					for (int i = 0; i < builder.Length; i++) //Insert ' ' before and after '-'
 					{
-						if (builder[i + 1] != ' ')
+						if (builder[i] == '-')
 						{
-							builder.Insert(i + 1, ' ');
-						}
-						if (builder[i - 1] != ' ')
-						{
-							builder.Insert(i, ' ');
-						}
-					}
-				}
-
-				userString = builder.ToString();
-				words = userString.Split(separator, StringSplitOptions.RemoveEmptyEntries);
-				for (int i = 0; i < words.Length; i++)  //Upper first letter
-				{
-					if (words[i].Length > 1)
-					{
-						words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1, words[i].Length - 1).ToLower();
-					}
-					else
-					{
-						words[i] = words[i].ToUpper();
-					}
-				}
-
-				for (int i = 0; i < words.Length; i++)  //Include exeption (article e.t.)
-				{
-					for (int j = 0; j < exception.Length; j++)
-					{
-						if (words[i].Equals(exception[j]))
-						{
-							words[i] = words[i].ToLower();
+							if (builder[i + 1] != ' ')
+							{
+								builder.Insert(i + 1, ' ');
+							}
+							if (builder[i - 1] != ' ')
+							{
+								builder.Insert(i, ' ');
+							}
 						}
 					}
-				}
 
-				words[0] = words[0].Substring(0, 1).ToUpper() + words[0].Substring(1, words[0].Length - 1); //If article is the first word in the sentence
-				for (int i = 1; i < words.Length; i++)  //Upper first word in the sentence
-				{
-					for (int x = 0; x < punctuationEndMarks.Length; x++)
+					userString = builder.ToString();
+					words = userString.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+					for (int i = 0; i < words.Length; i++)  //Upper first letter
 					{
-						if (i + 1 < words.Length && words[i].Last() == punctuationEndMarks[x])
+						if (words[i].Length > 1)
 						{
-
-							words[i + 1] = words[i + 1].Substring(0, 1).ToUpper() + words[i + 1].Substring(1, words[i + 1].Length - 1);
-							break;
+							words[i] = words[i].Substring(0, 1).ToUpper() + words[i].Substring(1, words[i].Length - 1).ToLower();
 						}
+						else
+						{
+							words[i] = words[i].ToUpper();
+						}
+					}
 
+					for (int i = 0; i < words.Length; i++)  //Include exeption (article e.t.)
+					{
+						for (int j = 0; j < exception.Length; j++)
+						{
+							if (words[i].Equals(exception[j]))
+							{
+								words[i] = words[i].ToLower();
+							}
+						}
+					}
+
+					words[0] = words[0].Substring(0, 1).ToUpper() + words[0].Substring(1, words[0].Length - 1); //If article is the first word in the sentence
+					for (int i = 1; i < words.Length; i++)  //Upper first word in the sentence
+					{
+						for (int x = 0; x < punctuationEndMarks.Length; x++)
+						{
+							if (i + 1 < words.Length && words[i].Last() == punctuationEndMarks[x])
+							{
+
+								words[i + 1] = words[i + 1].Substring(0, 1).ToUpper() + words[i + 1].Substring(1, words[i + 1].Length - 1);
+								break;
+							}
+
+						}
+					}
+					userString = String.Join(" ", words);
+					Console.ForegroundColor = ConsoleColor.Gray;
+					Console.Write("Correct string: ");
+					Console.ForegroundColor = ConsoleColor.Green;
+					Console.WriteLine(userString);
+					Console.WriteLine();
+					Console.ForegroundColor = ConsoleColor.Gray;
+					if (args.Length != 0)
+					{
+						break;
 					}
 				}
-				userString = String.Join(" ", words);
-				Console.ForegroundColor = ConsoleColor.Gray;
-				Console.Write("Correct string: ");
-				Console.ForegroundColor = ConsoleColor.Green;
-				Console.WriteLine(userString);
-				Console.WriteLine();
-				Console.ForegroundColor = ConsoleColor.Gray;
-				if (args.Length != 0)
+				else
 				{
-					break;
+					Console.WriteLine("You didn't enter string!");
+					Console.ForegroundColor = ConsoleColor.Gray;
 				}
 			} while (true);
 		}
